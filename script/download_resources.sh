6a3cc87fe3fea874350740c00a58ddd50e07d146
Bachkukkik <bachkukkik@users.noreply.github.com>
2020-11-24 23:48:43 +0700

    Create download_resources.sh

---
+++ script/download_resources.sh
@@ -0,0 +1,12 @@
+git clone https://github.com/bachkukkik/remi.git
+cd remi
+gdown https://drive.google.com/uc?id=1gxuTSkF51NP04JZgTE46Pg4KQsbHQKGo
+gdown https://drive.google.com/uc?id=1nAKjaeahlzpVAX0F9wjQEG_hL4UosSbo
+gdown https://drive.google.com/uc?id=1JUDHGrVYGyHtjkfI2vgR1xb2oU8unlI3
+gdown https://drive.google.com/uc?id=1lpwRjijZ7WSu_DFQmc8duuiEOR5lslhr
+unzip REMI-tempo-checkpoint.zip
+rm REMI-tempo-checkpoint.zip
+unzip REMI-tempo-chord-checkpoint.zip
+rm REMI-tempo-chord-checkpoint.zip
+unzip ragtime_data.zip
+rm ragtime_data.zip
