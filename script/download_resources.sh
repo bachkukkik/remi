aa24eafeef616b48a26ad15ff733901739d7453a
Bachkukkik <bachkukkik@users.noreply.github.com>
2020-11-25 01:48:08 +0700

    Update download_resources.sh

--- script/download_resources.sh
+++ script/download_resources.sh
@@ -4,9 +4,12 @@ gdown https://drive.google.com/uc?id=1gxuTSkF51NP04JZgTE46Pg4KQsbHQKGo
 gdown https://drive.google.com/uc?id=1nAKjaeahlzpVAX0F9wjQEG_hL4UosSbo
 gdown https://drive.google.com/uc?id=1JUDHGrVYGyHtjkfI2vgR1xb2oU8unlI3
 gdown https://drive.google.com/uc?id=1lpwRjijZ7WSu_DFQmc8duuiEOR5lslhr
+gdown https://drive.google.com/uc?id=19mExDS4bx2jZGKgwv433DAdH293iXZLz
 unzip REMI-tempo-checkpoint.zip
 rm REMI-tempo-checkpoint.zip
 unzip REMI-tempo-chord-checkpoint.zip
 rm REMI-tempo-chord-checkpoint.zip
 unzip ragtime_data.zip
 rm ragtime_data.zip
+unzip kukkik-finetuned-checkpoint.zip
+rm kukkik-finetuned-checkpoint.zip
